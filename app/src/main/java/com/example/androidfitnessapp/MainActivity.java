16219a38cf79344a055686ab72dc13077565eb69
G-ZiggY <zhygius@gmail.com>
2021-05-19 12:11:39 +0100

    notification

--- app/src/main/java/com/example/androidfitnessapp/MainActivity.java
+++ app/src/main/java/com/example/androidfitnessapp/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             CharSequence name = "UserReminderChannel";
-            String description = "Visit the app and check workout schedule";
+            String description = "User reminder to workout";
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel("notifyUser", name, importance);
             channel.setDescription(description);
@@ -41,13 +41,16 @@ public class MainActivity extends AppCompatActivity {
 
             Calendar date = Calendar.getInstance();
             date.setTimeInMillis(System.currentTimeMillis());
-            date.set(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_WEEK_IN_MONTH), 7, 0);
+//            date.set(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_WEEK_IN_MONTH), 7, 0);
+            date.add(Calendar.MINUTE, 1);
+            date.set(Calendar.SECOND, 0);
 
             long triggerAt = date.getTimeInMillis();
-//            long repeatAfter = 60 * 1000;
+            long repeatAfter = 60 * 1000;
 
 //            alarmManager.set(AlarmManager.RTC_WAKEUP, triggerAt + repeatAfter, pendingIntent);
-            alarmManager.setRepeating(AlarmManager.RTC, triggerAt, AlarmManager.INTERVAL_DAY, pendingIntent);
+//            alarmManager.setRepeating(AlarmManager.RTC, triggerAt, AlarmManager.INTERVAL_DAY, pendingIntent);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, triggerAt, repeatAfter, pendingIntent);
         }
     }
 }
